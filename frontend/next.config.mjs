e9e8d927340fbb6e2c9a174ccd4d7d9fe2f21a3a
Godwins Tuyishime <gtuyishime02@gmail.com>
2025-07-15 12:16:27 -0400

    rerouting fix

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,30 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     rewrites: async () => {
-        return [
-            {
-                source: "/api/:path*",
-                destination:
-                    process.env.NODE_ENV === "development"
-                        ? "http://127.0.0.1:8000/api/:path*"
-                        : "/api/:path*",
-            },
-            {
-                source: "/docs",
-                destination:
-                    process.env.NODE_ENV === "development"
-                        ? "http://127.0.0.1:8000/docs"
-                        : "/api/docs",
-            },
-            {
-                source: "/openapi.json",
-                destination:
-                    process.env.NODE_ENV === "development"
-                        ? "http://127.0.0.1:8000/openapi.json"
-                        : "/api/openapi.json",
-            },
-        ];
-      },
+        // Only use rewrites in development
+        if (process.env.NODE_ENV === "development") {
+            return [
+                {
+                    source: "/api/:path*",
+                    destination: "http://127.0.0.1:8000/api/:path*",
+                },
+                {
+                    source: "/docs",
+                    destination: "http://127.0.0.1:8000/docs",
+                },
+                {
+                    source: "/openapi.json",
+                    destination: "http://127.0.0.1:8000/openapi.json",
+                },
+            ];
+        }
+        return [];
+    },
 };
 
 export default nextConfig;

f618e32b5125ff0a1a242364797457b35cea3127
Godwins Tuyishime <gtuyishime02@gmail.com>
2025-07-15 12:28:26 -0400

    python version error again

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 # Add the backend directory to the path
-sys.path.append(os.path.join(os.path.dirname(__file__), 'backend'))
+sys.path.append('backend')
 
 from app.main import app
 
